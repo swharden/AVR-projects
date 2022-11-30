289d5fd5791fd4a7809e039d819f136101547a3a
Scott W Harden <swharden@gmail.com>
2022-11-29 23:17:09 -0500

    Update bmp280.ino

--- Arduino BMP280 pressure sensor/bmp280/bmp280.ino
+++ Arduino BMP280 pressure sensor/bmp280/bmp280.ino
@@ -6,9 +6,20 @@ int16_t DEVICE_ADDRESS = 0x76;
 
 DigitLedDisplay ld = DigitLedDisplay(7, 6, 5);
 
+int32_t t_fine;
 uint16_t dig_T1;
 int16_t dig_T2;
 int16_t dig_T3;
+
+uint16_t dig_P1;
+int16_t dig_P2;
+int16_t dig_P3;
+int16_t dig_P4;
+int16_t dig_P5;
+int16_t dig_P6;
+int16_t dig_P7;
+int16_t dig_P8;
+int16_t dig_P9;
 
 void setup() {
   Serial.begin(115200);
@@ -23,23 +34,36 @@ void loop() {
   setupConfigRegister();
   setupControlRegister();
   waitForMeasurement();
-  long pres = getPressure();
-  long temp = getTemperature();
+
+  uint32_t temp = getTemperature();
+  float tempF = convertTemperature(temp);
+
+  uint32_t pres = getPressure();
+  uint32_t presConv = convertPressure(pres);
+  float presPa = presConv / 256.0;
+  float presPSI = presPa / 6894.76;
 
   ld.printDigit(temp);
 
-  Serial.print(convertTemperature(temp));
+  Serial.print(presPa);
   Serial.print(',');
-  Serial.print(temp);
-  Serial.print(',');
-  Serial.print(pres);
+  Serial.print(tempF);
   Serial.println();
 }
 
-void readCalibrationData(){
+void readCalibrationData() {
   dig_T1 = ReadInt16_LE(0x88);
   dig_T2 = ReadInt16_LE(0x8A);
   dig_T3 = ReadInt16_LE(0x8C);
+  dig_P1 = ReadInt16_LE(0x8E);
+  dig_P2 = ReadInt16_LE(0x90);
+  dig_P3 = ReadInt16_LE(0x92);
+  dig_P4 = ReadInt16_LE(0x94);
+  dig_P5 = ReadInt16_LE(0x96);
+  dig_P6 = ReadInt16_LE(0x98);
+  dig_P7 = ReadInt16_LE(0x9A);
+  dig_P8 = ReadInt16_LE(0x9C);
+  dig_P9 = ReadInt16_LE(0x9E);
 }
 
 void waitForMeasurement() {
@@ -78,7 +102,7 @@ void setupControlRegister() {
   Wire.endTransmission();
 }
 
-long getPressure() {
+uint32_t getPressure() {
   Wire.beginTransmission(DEVICE_ADDRESS);
   Wire.write(0xF7);
   Wire.endTransmission();
@@ -88,15 +112,15 @@ long getPressure() {
   byte press_lsb = Wire.read();
   byte press_xlsb = Wire.read();
 
-  long result;
-  result += (long)press_msb << 16;
-  result += (long)press_lsb << 8;
-  result += (long)press_xlsb;
+  uint32_t result;
+  result += (uint32_t)press_msb << 16;
+  result += (uint32_t)press_lsb << 8;
+  result += (uint32_t)press_xlsb;
 
   return result;
 }
 
-int16_t ReadInt16_LE(byte address){
+int16_t ReadInt16_LE(byte address) {
   Wire.beginTransmission(DEVICE_ADDRESS);
   Wire.write(address);
   Wire.endTransmission();
@@ -108,7 +132,7 @@ int16_t ReadInt16_LE(byte address){
   return (msb << 8) | (lsb);
 }
 
-long getTemperature() {
+uint32_t getTemperature() {
   Wire.beginTransmission(DEVICE_ADDRESS);
   Wire.write(0xFA);
   Wire.endTransmission();
@@ -118,10 +142,10 @@ long getTemperature() {
   byte temp_lsb = Wire.read();
   byte temp_xlsb = Wire.read();
 
-  long result;
-  result += (long)temp_msb << 16;
-  result += (long)temp_lsb << 8;
-  result += (long)temp_xlsb;
+  uint32_t result;
+  result += (uint32_t)temp_msb << 16;
+  result += (uint32_t)temp_lsb << 8;
+  result += (uint32_t)temp_xlsb;
 
   return result;
 }
@@ -130,8 +154,26 @@ float convertTemperature(long adc_T) {
   adc_T >>= 4;
   int32_t var1 = ((((adc_T >> 3) - ((int32_t)dig_T1 << 1))) * ((int32_t)dig_T2)) >> 11;
   int32_t var2 = (((((adc_T >> 4) - ((int32_t)dig_T1)) * ((adc_T >> 4) - ((int32_t)dig_T1))) >> 12) * ((int32_t)dig_T3)) >> 14;
-  int32_t t_fine = var1 + var2;
+  t_fine = var1 + var2;
   float T = (t_fine * 5 + 128) >> 8;
-  T = T * 9 / 5 + 3200; // C to F
+  T = T * 9 / 5 + 3200;  // C to F
   return T / 100;
+}
+
+uint32_t convertPressure(int32_t adc_P) {
+  adc_P >>= 4;
+  int64_t var1, var2, p;
+  var1 = ((int64_t)t_fine) - 128000;
+  var2 = var1 * var1 * (int64_t)dig_P6;
+  var2 = var2 + ((var1 * (int64_t)dig_P5) << 17);
+  var2 = var2 + (((int64_t)dig_P4) << 35);
+  var1 = ((var1 * var1 * (int64_t)dig_P3) >> 8) + ((var1 * (int64_t)dig_P2) << 12);
+  var1 = (((((int64_t)1) << 47) + var1)) * ((int64_t)dig_P1) >> 33;
+  if (var1 == 0) return 0;
+  p = 1048576 - adc_P;
+  p = (((p << 31) - var2) * 3125) / var1;
+  var1 = (((int64_t)dig_P9) * (p >> 13) * (p >> 13)) >> 25;
+  var2 = (((int64_t)dig_P8) * p) >> 19;
+  p = ((p + var1 + var2) >> 8) + (((int64_t)dig_P7) << 4);
+  return (float)p;
 }
