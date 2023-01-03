65d2352811a2d93613b872cf5c048fc0c1e21df9
Scott W Harden <swharden@gmail.com>
2023-01-02 21:13:30 -0500

    AVR64DD32 LEDblink

---
+++ AVR64DD32 LEDblink/LEDblink/main.c
@@ -0,0 +1,17 @@
+#define F_CPU 3333333UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	PORTD.DIR = 255;
+	
+    while (1) 
+    {
+		PORTD.OUT = 255;
+		_delay_ms(100);
+		PORTD.OUT = 0;
+		_delay_ms(900);
+    }
+}
+
